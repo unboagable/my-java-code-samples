f0aecb20330620ec9585b3f3cf1cd04e344e9ae0
chang-hyunmungai <chang-hyunmungai@localhost>
2024-06-19 13:48:48 -0400

    Added more code

---
+++ Code Practice/src/solutions/family_tree/FamilyTree.java
@@ -0,0 +1,9 @@
+package solutions.family_tree;
+
+import structures.BinaryTreeNode;
+
+public class FamilyTree {
+	public static BinaryTreeNode familyTree(FamilyDemographic fd, string name) {
+		BinaryTreeNode(x)
+	}
+}
--- Code Practice/src/structures/BinaryTreeNode.java
+++ Code Practice/src/structures/BinaryTreeNode.java
@@ -2,6 +2,7 @@ package structures;
 
 public class BinaryTreeNode {
 	public int val;
+	public Object data;
 	public BinaryTreeNode left;
 	public BinaryTreeNode right;
 	
@@ -9,5 +10,12 @@ public class BinaryTreeNode {
 		val=x;
 		left=null;
 		right=null;
+	}
+	
+	public BinaryTreeNode(int x, Object input){
+		val=x;
+		left=null;
+		right=null;
+		data=input;
 	}
 }
