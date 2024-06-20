f6608ee50d8a4833c1a96a78aab98ae139a8014e
chang-hyunmungai <chang-hyunmungai@localhost>
2024-06-19 21:44:28 -0400

    Scramble

--- Code Practice/src/solutions/family_tree/FamilyTree.java
+++ Code Practice/src/solutions/family_tree/FamilyTree.java
@@ -1,9 +1,30 @@
 package solutions.family_tree;
 
 import structures.BinaryTreeNode;
+import structures.family_tree.FamilyDemograpgic;
 
 public class FamilyTree {
-	public static BinaryTreeNode familyTree(FamilyDemographic fd, string name) {
-		BinaryTreeNode(x)
+	public static BinaryTreeNode familyTree(FamilyDemograpgic fd, String name, int size) {
+		System.out.print("size is.. ");
+		System.out.println(size);
+		if(size == 0) { //no name
+			System.out.println("anonymous");
+			return null;
+		}else if(size == 1) { //one person
+			System.out.println("single");
+			return new BinaryTreeNode(1, name);
+		}else if (size == 2) { // single parent
+			System.out.println("one parent");
+			return new BinaryTreeNode(1, name);
+		}else if (size == 3) { // family
+			System.out.println("one parent");
+			return new BinaryTreeNode(1, name);
+		}else if(size == 5) { // community
+			System.out.println("family");
+			return new BinaryTreeNode(1, name);
+		}else {
+			System.out.println("family");
+			return new BinaryTreeNode(1, name);
+		}
 	}
 }
--- Code Practice/src/structures/family_tree/FamilyDemograpgic.java
+++ Code Practice/src/structures/family_tree/FamilyDemograpgic.java
@@ -1,20 +1,12 @@
 package structures.family_tree;
 
-import structures.BinaryTreeNode;
-
 public final class FamilyDemograpgic {
 	int type = 0; //0 = orphan, 1 = single family, 2 = full family
 	
-	public static BinaryTreeNode FamilyDemograpgic(int dem) {
-		int error = dem % 2;
-		if (error == 0) {
-			BinaryTreeNode(dem);
-		};
-		return null;
-	}
-
-	private static void BinaryTreeNode(int dem) {
-		// TODO Auto-generated method stub
-		
+	public FamilyDemograpgic(int dem){
+		if(dem % 2 >= 2) {
+			type = -1;
+		}
+		type = dem;
 	}
 }
