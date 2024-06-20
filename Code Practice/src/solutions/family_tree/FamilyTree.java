3e9f621d3a85071f47063046e664286dd19c9752
chang-hyunmungai <chang-hyunmungai@localhost>
2024-06-19 21:59:54 -0400

    Update demographics

--- Code Practice/src/solutions/family_tree/FamilyTree.java
+++ Code Practice/src/solutions/family_tree/FamilyTree.java
@@ -4,27 +4,27 @@ import structures.BinaryTreeNode;
 import structures.family_tree.FamilyDemograpgic;
 
 public class FamilyTree {
-	public static BinaryTreeNode familyTree(FamilyDemograpgic fd, String name, int size) {
+	public static BinaryTreeNode familyTree(FamilyDemograpgic fd, String name, int place) {
 		System.out.print("size is.. ");
-		System.out.println(size);
-		if(size == 0) { //no name
+		System.out.println(place);
+		if(place == 0) { //no name
 			System.out.println("anonymous");
 			return null;
-		}else if(size == 1) { //one person
+		}else if(place == 1) { //one person
 			System.out.println("single");
 			return new BinaryTreeNode(1, name);
-		}else if (size == 2) { // single parent
+		}else if (place == 2) { // single parent
 			System.out.println("one parent");
-			return new BinaryTreeNode(1, name);
-		}else if (size == 3) { // family
+			return new BinaryTreeNode(2, name);
+		}else if (place == 3) { // family
 			System.out.println("one parent");
-			return new BinaryTreeNode(1, name);
-		}else if(size == 5) { // community
+			return new BinaryTreeNode(3, name);
+		}else if(place == 5) { // community
 			System.out.println("family");
-			return new BinaryTreeNode(1, name);
+			return new BinaryTreeNode(5, name);
 		}else {
 			System.out.println("family");
-			return new BinaryTreeNode(1, name);
+			return new BinaryTreeNode(place, name);
 		}
 	}
 }
--- Code Practice/src/structures/BinaryTreeNode.java
+++ Code Practice/src/structures/BinaryTreeNode.java
@@ -1,5 +1,7 @@
 package structures;
 
+import java.util.List;
+
 public class BinaryTreeNode {
 	public int val;
 	public Object data;
@@ -17,5 +19,18 @@ public class BinaryTreeNode {
 		left=null;
 		right=null;
 		data=input;
+	}
+	
+	public BinaryTreeNode BinaryTreeNode(int x, Object input, int size, List<Object> others){
+		val=x;
+		if (size == 0) {
+			return null;
+		}else if (size == 1) {
+			return new BinaryTreeNode(x, input);
+		}
+		left=null;
+		right=null;
+		data=input;
+		return null;
 	}
 }
